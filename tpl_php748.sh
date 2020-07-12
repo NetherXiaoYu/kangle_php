8ea013a28ba8c420c684f6e1ebd14b0db79f14cc
NetherXiaoYu <2031464675@qq.com>
2020-07-12 19:31:11 +0800

    Update tpl_php748.sh

--- tpl_php748.sh
+++ tpl_php748.sh
@@ -1,4 +1,4 @@
-yum -y install gcc gcc-c++ make cmake bison autoconf wget lrzsz libtool libtool-ltdl-devel freetype-devel libjpeg.x86_64 libjpeg-devel libpng-devel gd-devel python-devel  patch  sudo openssl* openssl openssl-devel ncurses-devel bzip* bzip2 unzip zlib-devel libevent* libxml* libxml2-devel libcurl* curl-devel readline-devel sqlite-devel libsodium oniguruma oniguruma-devel
+yum -y install sqlite-devel libsodium oniguruma oniguruma-devel
 wget -O tpl_php748.tar.gz https://raw.githubusercontent.com/NetherXiaoYu/kangle_php/master/tpl_php748.tar.gz
 tar zxvf tpl_php748
 mv tpl_php748 /vhs/kangle/ext
