0c9297096d2b1f852c533dd954a2e26add2f84ba
NetherXiaoYu <58361779+NetherXiaoYu@users.noreply.github.com>
2020-12-05 14:05:54 +0800

    Create tpl_php800.sh

---
+++ tpl_php800.sh
@@ -0,0 +1,6 @@
+yum -y install sqlite-devel libsodium oniguruma oniguruma-devel
+wget -O tpl_php748.tar.gz https://raw.githubusercontent.com/NetherXiaoYu/kangle_php/master/tpl_php800.tar.gz
+tar zxvf tpl_php800.tar.gz
+mv tpl_php800 /vhs/kangle/ext
+rm -rf /tmp/*
+/vhs/kangle/bin/kangle -r
